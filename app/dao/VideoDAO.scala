ad9617ee919fac5c55781a7f76d345b29d8370ad
Pranav Sathyanarayanan <sathyp@rpi.edu>
2015-11-20 01:27:37 -0500

    Added caption query, processing works, we gucci

--- app/dao/VideoDAO.scala
+++ app/dao/VideoDAO.scala
@@ -38,8 +38,9 @@ class VideoDAO extends GenericDAO with VideoComponent {
     val action = sql"""SELECT FLOOR(frame * 24 * $duration::numeric::integer / $frames::numeric::integer) AS frames
                        FROM inq_video_metadata
                        WHERE id = $id
-                       AND image_data LIKE $keywords
-                       OR ocr_data LIKE $keywords""".as[Long]
+                       AND LOWER(image_data) LIKE $keywords
+                       OR LOWER(ocr_data) LIKE $keywords
+                       OR LOWER(caption) LIKE $keywords""".as[Long]
 
     db.run(action.asTry).map {
       case Success(data: Seq[Long]) => data
